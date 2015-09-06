931a90880cb0c756c07041e429b62fb76b9bcfe1
Cheng-Yu Lin <ntuaha@gmail.com>
2015-09-07 00:59:51 +0800

    顯示前10大用語

--- src/analysis.R
+++ src/analysis.R
@@ -4,19 +4,14 @@ by_age = by(data,data$age,function(d) table(d$word))
 filter_age = sapply(by_age,function(d) unlist(d[d>1]))
 
 getImportant = function(d){
-  t = unlist(d)
-  ranks = rank(-t)
-  ans = c(rownames(t)[ranks==1],rownames(t)[ranks==2],rownames(t)[ranks==3],rownames(t)[ranks==4],rownames(t)[ranks==5])
-  return(ans)
+  return (rownames(sort(-unlist(d)))[1:10])
 }
-
-important_name = sapply(filter_age,getImportant)
+important_name = lapply(filter_age,getImportant)
 
 writeout = function(n){
   cdata = important_name[[n]]
-  cdata = t(c(n,cdata))
+  cdata = t(c(n,cdata,rep("",len=10))[1:11])
   write.table(cdata,file="./output.csv",sep=",",append = TRUE,col.names=F,row.names = FALSE)
 }
-#important_name$name = NULL
 sapply(names(important_name),writeout)
 
